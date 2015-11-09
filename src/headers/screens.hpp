594e88b1a515b97e4099e55fad425fbaa386038b
Jenni Pajukoski <jenni@giosg.com>
2015-11-09 15:36:19 +0200

    Added missing screens.hpp

---
+++ src/headers/screens.hpp
@@ -0,0 +1,11 @@
+#ifndef SCREENS_HPP_INCLUDED
+#define SCREENS_HPP_INCLUDED
+
+//Basic Screen Class
+#include "base_screen.hpp"
+
+//Including each screen of application
+#include "menu_screen.hpp"
+#include "game_screen.hpp"
+
+#endif
