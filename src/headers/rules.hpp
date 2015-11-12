255aebef0a977496266e32f4fc8d8dd73f00c874
Antti <antti.3.niemi@aalto.fi>
2015-11-12 19:19:20 +0200

    Added rules for pawns
    Added rules for both white and black pawns
    and the rules header file (rules.hpp)

---
+++ src/headers/rules.hpp
@@ -0,0 +1,24 @@
+#ifndef CHESS_RULES_H
+#define CHESS_RULES_H
+
+#include <map>
+#include <vector>
+
+
+namespace Rules {
+
+
+	std::vector<int> whitePawnMoveForward(const std::vector<int>&, int);
+	std::vector<int> whitePawnMoveForwardLeft(const std::vector<int>&, int, const std::pair<int,int>&);
+	std::vector<int> whitePawnMoveForwardRight(const std::vector<int>&, int, const std::pair<int,int>&);
+
+
+	std::vector<int> blackPawnMoveForward(const std::vector<int>&, int);
+	std::vector<int> blackPawnMoveForwardLeft(const std::vector<int>&, int, const std::pair<int,int>&);
+	std::vector<int> blackPawnMoveForwardRight(const std::vector<int>&, int, const std::pair<int,int>&);
+
+
+}
+
+
+#endif
