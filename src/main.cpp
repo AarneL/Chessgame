c67e6353a22db544c76dc8ed43774f223c489638
Jenni Pajukoski <jenni@giosg.com>
2015-12-07 20:46:33 +0200

    Changed back to 1200x800 window

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,18 @@ int main(int argc, char** argv)
 	std::vector<BaseScreen*> screens;
 
 	// Window creation
-	std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
-	for (std::size_t i = 0; i < modes.size(); ++i)
-	{
-		sf::VideoMode mode = modes[i];
-		std::cout << "Mode #" << i << ": "
-              << mode.width << "x" << mode.height << " - "
-              << mode.bitsPerPixel << " bpp" << std::endl;
-	}
+	// std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
+	// for (std::size_t i = 0; i < modes.size(); ++i)
+	// {
+	// 	sf::VideoMode mode = modes[i];
+	// 	std::cout << "Mode #" << i << ": "
+ //              << mode.width << "x" << mode.height << " - "
+ //              << mode.bitsPerPixel << " bpp" << std::endl;
+	// }
 
-	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-	sf::RenderWindow window(sf::VideoMode(modes[2]), "Chess game", 4); // Last parameter makes window unresizable
+	// sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+	// sf::RenderWindow window(sf::VideoMode(modes[2]), "Chess game", 4); // Last parameter makes window unresizable
+	sf::RenderWindow window(sf::VideoMode(1200, 800), "Chess game", 4);
 
 	// Screens
 	GameScreen gameScreen(window);
